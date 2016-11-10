35906c00e13c1cfaeaf7b07bf5d57ee054d88eb1
x359x <nuken_96@mail.ru>
2016-11-10 17:22:22 +0300

    7. Temporary UI state

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,13 +1,28 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
  
 import { Tasks } from '../api/tasks.js';
+
 import './body.html';
 import './task.js';
  
+ Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
   tasks() {
+	      const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
        // Show newest tasks at the top
     return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+    incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
   },
 });
 
@@ -28,5 +43,8 @@ Template.body.events({
  
     // Clear form
     target.text.value = '';
+  },
+   'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
   },
 });
